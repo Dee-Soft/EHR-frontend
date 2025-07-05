fa0be3825018559d4573667475c2ec99d3a182e4
Sarfaraj84 <dee.dev.au@gmail.com>
2025-07-05 15:27:35 +1000

    Created all login pages

--- src/app/auth/login/employee/page.tsx
+++ src/app/auth/login/employee/page.tsx
@@ -1,1 +1,7 @@
-import { loginSchema, LoginSchema } from '@/schemas/authSchemas';
+'use client';
+
+import LoginForm from '@/components/forms/LoginForm';
+
+export default function EmployeeLoginPage() {
+  return <LoginForm role="Employee" />;
+}
